5f550ae11e397d35941ad6142baec4aaed7e53b4
Seth Fair <seth@sethfair.com>
2023-09-12 00:39:07 -0500

    add new plugins

--- lua/user/plugins/dressing.lua
+++ lua/user/plugins/dressing.lua
@@ -1,4 +1,24 @@
 return {
-  "stevearc/dressing.nvim",
-  event = "VeryLazy"
+    "stevearc/dressing.nvim",
+    event = "VeryLazy",
+    opts = {
+        input = {
+            win_options = {
+                winblend = 0,
+            },
+        },
+        select = {
+            backend = { "fzf_lua", "fzf", "builtin", "nui" },
+            nui = {
+                win_options = {
+                    winblend = 0,
+                },
+            },
+            builtin = {
+                win_options = {
+                    winblend = 0,
+                },
+            },
+        },
+    },
 }
